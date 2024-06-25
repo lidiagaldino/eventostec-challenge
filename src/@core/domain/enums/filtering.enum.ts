5e2f1d2c3270b60486d48315ce0d8d0570d6fd16
lidiagaldino <lidiagabrielly14@gmail.com>
2024-06-25 20:20:38 -0300

    feat: Add enums for filter rules and user roles

---
+++ src/@core/domain/enums/filtering.enum.ts
@@ -0,0 +1,16 @@
+// valid filter rules
+export enum FilterRule {
+  EQUALS = 'eq',
+  NOT_EQUALS = 'neq',
+  GREATER_THAN = 'gt',
+  GREATER_THAN_OR_EQUALS = 'gte',
+  LESS_THAN = 'lt',
+  LESS_THAN_OR_EQUALS = 'lte',
+  LIKE = 'like',
+  NOT_LIKE = 'nlike',
+  IN = 'in',
+  CONTAINS = 'contains',
+  NOT_IN = 'nin',
+  IS_NULL = 'isnull',
+  IS_NOT_NULL = 'isnotnull',
+}
